d5f2eb6910322685819b958dcf97a1b07ed54c5d
Seth Hobson <sethlawrencehobson@gmail.com>
2023-12-22 19:20:50 -0500

    added add/edit form

--- forms.py
+++ forms.py
@@ -1,1 +1,40 @@
 """Forms for Flask Cafe."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, HiddenField, URLField, SelectField
+from wtforms.validators import InputRequired, Email, Length, URL, Optional
+from models import City
+
+
+class AddOrEditCafe(FlaskForm):
+    """form for adding or editing cafe"""
+
+    name = StringField(
+        'name',
+        validators=[InputRequired()]
+    )
+
+    description = StringField(
+        'description',
+        validators=[Optional()]
+    )
+
+    url = URLField(
+        'url',
+        validators=[Optional(), URL()]
+    )
+
+    address = StringField(
+        'adddress',
+        validators=[InputRequired()]
+    )
+
+    city_code = SelectField(
+        'city code',
+        choices=[(x, x) for x in City.query.all()]
+    )
+
+    image_url = URLField(
+        'url',
+        validators=[URL(), Optional()]
+    )
